79c03a019b4e5381f94ee6dfe7583ece9ca200dd
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-02-18 08:42:32 -0500

    header and footer, timestamp 1:04:30

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,13 +1,17 @@
-import femaleProfile from './images/femaleProfile.jpg'
-import maleProfile from './images/maleProfile.jpg'
+import femaleProfile from "./images/femaleProfile.jpg";
+import maleProfile from "./images/maleProfile.jpg";
 
-const Employees = () => {
+const Employees = ({employees, selectedTeam, handleEmployeeCardClick,  handleTeamSelectionChange}) => {
   
   return (
     <main className="container">
       <div className="row justify-content-center mt-3 mb-3">
         <div className="col-6">
-          <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
+          <select
+            className="form-select form-select-lg"
+            value={selectedTeam}
+            onChange={handleTeamSelectionChange}
+          >
             <option value="TeamA">Team A</option>
             <option value="TeamB">Team B</option>
             <option value="TeamC">Team C</option>
@@ -17,25 +21,36 @@ const Employees = () => {
       </div>
       <div className="row justify-content-center mt-3 mb-3">
         <div className="col-8">
-      
           <div className="card-collection">
-            {
-              employees.map((employee) => (
-                <div id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{cursor:"pointer"}} onClick={handleEmployeeCardClick}>                    
-                  {(employee.gender === 'male')?<img src={maleProfile} className="card-img-top"/>
-                    : <img src={femaleProfile} className="card-img-top"/>}
-                  
-                  <div className="card-body">
-                      <h5 className="card-title">Full Name: {employee.fullName}</h5>
-                      <p className="card-text">Designation: {employee.designation}</p>
-                    </div>
+            {employees.map((employee) => (
+              <div
+                id={employee.id}
+                className={
+                  employee.teamName === selectedTeam
+                    ? "card m-2 standout"
+                    : "card m-2"
+                }
+                style={{ cursor: "pointer" }}
+                onClick={handleEmployeeCardClick}
+              >
+                {employee.gender === "male" ? (
+                  <img src={maleProfile} className="card-img-top" />
+                ) : (
+                  <img src={femaleProfile} className="card-img-top" />
+                )}
+
+                <div className="card-body">
+                  <h5 className="card-title">Full Name: {employee.fullName}</h5>
+                  <p className="card-text">
+                    Designation: {employee.designation}
+                  </p>
                 </div>
-              ))
-            }
-            </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </main>
-  )
-}
-export default Employees
+  );
+};
+export default Employees;
