32be44a70b7571779c300c2a775738fdffe887bf
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-02-18 08:43:12 -0500

    header and footer 2, 1:04:30

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,9 @@ function App() {
 
   return (
     <div>
-      <Header/>
+      <Header selectedTeam={selectedTeam}
+              teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length}
+      />
       <Employees employees={employees}
                   selectedTeam={selectedTeam}
                   handleEmployeeCardClick={handleEmployeeCardClick}
