adf1ffd2660780a6132995c9af8c6f60df22b67c
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-02-20 20:02:17 -0500

    added routes and nav

---
+++ src/GroupedTeamMembers.jsx
@@ -0,0 +1,14 @@
+const GroupedTeamMembers = ({ selectedTeam, teamMemberCount }) => {
+
+    return (
+      <header className="container">
+        <div className="row justify-content-center mt-3 mb-4">
+          <div className="col-8">
+            <h1>Grouped Team Members</h1>
+          </div>
+        </div>
+      </header>
+    )
+  }
+  export default GroupedTeamMembers;
+  
